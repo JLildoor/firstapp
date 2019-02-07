531609408f0ba9e2aef79d2a7afc28cff3fcd650
Jason LaPorte <jason.laporte@me.com>
2019-02-07 14:54:03 -0500

    5.3 add contact with sendgrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,15 @@
 class SimplePagesController < ApplicationController
   def index
   end
-end
 
-class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email, to: 'jason.laporte@me.com', subject: "A new contact form message from #{@name}", body: @message).deliver_now
   end
 end
