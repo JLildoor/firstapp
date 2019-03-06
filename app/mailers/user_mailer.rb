620228bf334b9de0707be8bbefc6a6707323f186
Jason LaPorte <jason.laporte@me.com>
2019-03-06 00:31:49 -0500

    add user signup mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,12 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
   @message = message
-    mail(from: email, to: 'your-email@example.com', subject: "A new contact form message from #{name}")
+    mail(from: email, to: 'jason.laporte@me.com   ', subject: "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+    @appname = "Breakfast For Dinner Workshop"
+    mail(to: user.email, subject: "Welcome to #{@appname}!")
   end
 
   def thank_you
