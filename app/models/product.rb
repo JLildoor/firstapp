796492ebeacfa3e514e707b14c0ffd3902c1bdec
Jason LaPorte <jason.laporte@me.com>
2019-03-14 16:55:18 -0400

    add rspec and update actionview

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,6 +20,6 @@ def self.search(search_term)
   if Rails.environment.production?
     Product.where("name LIKE ? OR description LIKE ?", "%#{search_term}%", "%#{search_term}%")
   else
-    Product.where("name ilike ? OR description ilike ?", "%#{search_term}%"), "%#{search_term}%"
+    Product.where("name ilike ? OR description ilike ?", "%#{search_term}%", "%#{search_term}%")
   end
 end
